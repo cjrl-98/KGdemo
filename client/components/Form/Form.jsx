c0170b67d8994bd571be27d6a2bf83ffc52ff6d9
Kelly K H <kh.waterloo@gmail.com>
2020-04-07 00:25:27 -0400

    added validation to Form.jsx

--- client/components/Form/Form.jsx
+++ client/components/Form/Form.jsx
@@ -6,6 +6,13 @@ export default class Form extends React.Component{
 
   submitFunction(e){
     e.preventDefault();
+    let cc = e.target.ccNumber.value.replace(/\s/g, "");
+    let ccVal = /4[0-9]{12}(?:[0-9]{3})/g.test(cc);
+    if (!ccVal) {
+      console.log('do something');
+      return
+    }
+    console.log('posting cc value', cc);
   }
   
   render(){
@@ -15,7 +22,7 @@ export default class Form extends React.Component{
           <h1 class="form__title">Payment Details</h1>
           <form className="form__entry" onSubmit={e => {this.submitFunction(e)}}>
             <UpperForm />
-            <input type="" id="ccExpiry" name="ccExpiry" pattern="^(((0[5-9])|(1[0-2]))\/((20))|((0[1-9])|(1[0-2]))\/(2[1-9]))$" placeholder="MM/YY" required></input>
+            {/* <input type="" id="ccExpiry" name="ccExpiry" pattern="^(((0[5-9])|(1[0-2]))\/((20))|((0[1-9])|(1[0-2]))\/(2[1-9]))$" placeholder="MM/YY" required></input> */}
             <input type="submit" value="Submit"></input>
           </form> 
         </div>
